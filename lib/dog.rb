3c48dcaf9b1f2e64b38d0e1a1354510c37a6573f
Dawn Beattie <heartandhandstraining@gmail.com>
2018-12-14 21:13:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+# class definition
+class Dog
+  # body of Dog class
+  def initialize(breed)
+    @breed = breed
+  end
+  def breed= (breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  def bark # instance method
+    "woof!"
+  end
+end
+
+
